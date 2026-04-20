14c2dbd28c70fbecfff17332defcd97dd414771d
johnayuk <john1234ayuk@gmail.com>
2026-04-20 12:25:18 +0100

    change the url in the swagger.js and also added external allowed url to the app.js cors function

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -1,60 +1,3 @@
-// const swaggerJsdoc = require('swagger-jsdoc');
-
-// const options = {
-//   definition: {
-//     openapi: '3.0.0',
-//     info: {
-//       title: 'Corporate Investment Platform API',
-//       version: '1.0.0',
-//       description: 'Backend API for Corporate Investment Platform',
-//       contact: {
-//         name: 'API Support',
-//         email: 'support@corporateinvestment.com',
-//       },
-//     },
-//     servers: [
-//       {
-//         url: 'http://localhost:5000/api/v1',
-//         description: 'Development server',
-//       },
-//     ],
-//     components: {
-//       securitySchemes: {
-//         bearerAuth: {
-//           type: 'http',
-//           scheme: 'bearer',
-//           bearerFormat: 'JWT',
-//         },
-//       },
-//     },
-//     security: [
-//       {
-//         bearerAuth: [],
-//       },
-//     ],
-//   },
-//   apis: ['./src/routes/*.js'],
-// };
-
-// const swaggerSpec = swaggerJsdoc(options);
-
-// module.exports = { swaggerSpec };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const swaggerJsdoc = require('swagger-jsdoc');
 
 const options = {
@@ -76,26 +19,12 @@ const options = {
         url: 'http://localhost:5000/api/v1',
         description: 'Local Development',
       },
+
       {
-        url: 'http://localhost:3000/api/v1',
-        description: 'Alt Local (if proxying)',
+        url: 'https://cherry.dealdrivetechnology.com/api/v1',
+        description: 'staging',
       },
-      {
-        url: 'https://investmentwebsite-backend.onrender.com/api/v1',
-        description: 'Local IP',
-      },
-      {
-        url: 'https://dev-api.yourdomain.com/api/v1',
-        description: 'Development Server',
-      },
-      {
-        url: 'https://staging-api.yourdomain.com/api/v1',
-        description: 'Staging Server',
-      },
-      {
-        url: 'https://api.yourdomain.com/api/v1',
-        description: 'Production Server',
-      },
+
     ],
 
     components: {
